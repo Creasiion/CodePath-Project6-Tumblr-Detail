e4947126d9019e9e607eda876cbf327e85dc6be0
Creasion <129997790+Creasiion@users.noreply.github.com>
2024-04-02 00:20:54 -0700

    Add files via upload

---
+++ ios101-project6-tumblr/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  ios101-project6-tumblr
+//
+//  Created by Imani Cage on 4/1/24.
+//
+
+import UIKit
+import Nuke
+
+class DetailViewController: UIViewController {
+
+    var post: Post!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var navBarView: UINavigationItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        textView.text = post.caption.trimHTMLTags()
+        if let photo = post.photos.first {
+            let imageURL = photo.originalSize.url
+            Nuke.loadImage(with: imageURL, into: imageView)
+        }
+        
+        textView.textColor = .white
+        textView.backgroundColor = UIColor(red: 28/255, green: 74/255, blue: 155/255, alpha: 1.0)
+        view.backgroundColor = UIColor(red: 28/255, green: 74/255, blue: 155/255, alpha: 1.0)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
