602dbd3c58a779522312a27835793dc663856880
Creasion <129997790+Creasiion@users.noreply.github.com>
2024-04-02 00:21:52 -0700

    Update DetailViewController.swift

--- ios101-project6-tumblr/DetailViewController.swift
+++ ios101-project6-tumblr/DetailViewController.swift
@@ -14,7 +14,6 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var navBarView: UINavigationItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
